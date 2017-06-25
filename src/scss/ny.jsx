b6cb610c096e4d6120df93f92edd311caf8b21b2
Halvor Rønneseth <halvor.ronneseth@capgemini.com>
2017-06-25 22:13:22 +0200

    Ny i test

---
+++ src/scss/ny.jsx
@@ -0,0 +1,20 @@
+import React, {PropTypes} from 'react';
+
+export default class Ny extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>MyComponent</div>
+        <div>Ny comitt</div>
+      </div>
+    );
+  }
+}
+
+Ny.propTypes = {
+  ny = PropTypes.String
+};
