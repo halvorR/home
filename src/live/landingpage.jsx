573ec4a341c13f28bb0cc7c5778f7517980c7502
Halvor Rønneseth <halvor.ronneseth@capgemini.com>
2017-07-03 21:35:52 +0200

    Test i master

---
+++ src/live/landingpage.jsx
@@ -0,0 +1,12 @@
+// Live updates
+import React, { PropTypes } from 'react'
+
+const Landing = React.createClass({
+  render () {
+    return (
+      <div>Holter live</div>
+    )
+  }
+})
+
+export default Landing
